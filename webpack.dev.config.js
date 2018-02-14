396080c8c52c6afcb9d1a313bf3eca89779484f2
Noah <noah@oliverlanz.ch>
2018-02-14 15:38:05 +0000

    Changed webpack-dev-server config to wprk in Cloud9

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -56,7 +56,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        port: 9000,
+        // port: 9000,
         inline: true,
         watchOptions: {
             aggregateTimeout: 300,
