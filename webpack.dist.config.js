e5ee4fadc5dc024efbc1cdae0113ae0d1b4a55af
Oliver Lanz <oliver.lanz@gigglingbits.com>
2018-05-06 01:10:25 +0200

    Make it run also outside of C9

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -58,7 +58,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        //port: 9000,
+        port: 9000,
         inline: true,
         watchOptions: {
             aggregateTimeout: 300,
