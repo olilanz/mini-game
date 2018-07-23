17cf64bd04184df35b9db5cb12a659b36df4be1e
Oliver Lanz <oliver@Lychee.oliverlanz.int>
2018-07-23 19:22:45 +0200

    Added comment

--- src/assets.d.ts
+++ src/assets.d.ts
@@ -1,3 +1,13 @@
+/**
+ * The bumndlers (webpack or parcel, etc) work best for static assest if they are imported 
+ * in the TypeScript code. If done so, the bundlers will also include those imported static 
+ * assets in the build - hence the build will be complete.
+ * 
+ * However, importing these assets in TypScript causes VSCode to report TS errors. The following
+ * declarations render the imports as valid.
+ * 
+ * Based on post: https://github.com/Microsoft/TypeScript/issues/6615
+ */
 declare module "*.png" {
     const value: any;
     export default value;
