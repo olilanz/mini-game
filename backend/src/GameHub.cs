3da6215b9279cf680c1caf4a98c4764141fb0c35
Oliver Lanz <oliver@Olivers-MBP.oliverlanz.int>
2018-09-18 00:11:24 +0200

    Preparing connections to backend

--- backend/src/GameHub.cs
+++ backend/src/GameHub.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
-namespace Backend
-{
-    public class GameHub : Hub
-    {
-        public async Task SendMessage(string user, string message)
-        {
+namespace Backend {
+    public class GameHub : Hub {
+        public async Task SendMessage(string user, string message) {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -8,16 +8,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Backend
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace Backend {
+    public class Program {
+        public static void Main(string[] args) {
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => 
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
