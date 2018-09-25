8e5fe75479f9a87697684f481b7c67b41a9ca635
Oliver Lanz <oliver@Olivers-MBP.oliverlanz.int>
2018-09-25 08:42:25 +0200

    wip: Improving connection handling

--- frontend/src/communication/connection.ts
+++ frontend/src/communication/connection.ts
@@ -1,29 +1,67 @@
 import * as signalR from "@aspnet/signalr";
 
 export class Connection {
-    private _connect: signalR.HubConnection | undefined;
+    // todo: no thread safe access!
+    private _connection: signalR.HubConnection | undefined;
 
     constructor(url: string) {
         console.log("Initiating Server connection: " + url);
     }
 
-    connect(): boolean {
+    connect(): Promise<void> {
         let connection = new signalR.HubConnectionBuilder()
             .withUrl("/gamehub")
+            .configureLogging(signalR.LogLevel.Debug)
             .build();
 
-        connection.on("ReceiveMessage", (username: string, message: string) => {
-            console.log(`Incoming message from server: ${username} - ${message}`);
-        });   
+        this.attachEventHandlers(connection);
 
-        connection.start().catch(err => console.log(err)).then(
+        // todo: error handling
+        // detachEventHandlers(connection);
+        let promise = connection.start().catch(err => console.log(err)).then(
             function () {
                 console.log(`Sending message from server...`);
                 connection.send("sendMessage", "Gagalarage", "This is a small message from the client...");
             }
         );
               
-        return false;
+        // todo: only if all went well
+        this._connection = connection;
+
+        return promise;
+    }
+
+    disconnect(): void {
+        if (this._connection) {
+            this._connection.stop();
+        }
+    }
+
+    private attachEventHandlers(connection: signalR.HubConnection) {
+        connection.on("ReceiveMessage", (username: string, message: string) => {
+            console.log(`Incoming message from server: ${username} - ${message}`);
+        });   
+
+        connection.onclose(this.disconnectHandler);
+    }
+
+    private detachEventHandlers(connection: signalR.HubConnection) {
+        connection.on("ReceiveMessage", (username: string, message: string) => {
+            console.log(`Incoming message from server: ${username} - ${message}`);
+        });   
+
+        connection.onclose(this.disconnectHandler);
+    }
+
+    private disconnectHandler(error: Error | undefined): void {
+        if (error) {
+            console.log("Connection lost: " + error.name + " - " + error.message);
+        } else {
+            console.log("Connection lost: [undefined error]");
+        }
+        if (this._connection) {
+            this.detachEventHandlers(this._connection);
+        }
+        this._connection = undefined;
     }
 }
-
