3747ed609c956b0e1508e4e4438ef7c4b7a615db
Oliver Lanz <oliver@Lychee.local>
2019-01-26 13:12:17 +0100

    Fixed invalid warning on service worker registration

--- frontend/src/pwa-serviceworker-reg.js
+++ frontend/src/pwa-serviceworker-reg.js
@@ -5,16 +5,15 @@
  // inspired from https://alligator.io/js/service-workers/
 
 self.addEventListener('load', () => {
-  if (!('serviceWorker' in navigator)) {
-    console.warning('PWA support: service worker not supported 😣. Skipping PWA...');
-    return
+  if (('serviceWorker' in navigator)) {
+    navigator.serviceWorker.register('/pwa-serviceworker.js')
+    .then(function(registration) {
+      console.log('PWA support: Service worker successfully registered in scope 🏼:', registration);
+    })
+    .catch(function(error) {
+      console.error('PWA support: Service worker registration failed! 😱', error);
+    });
+  } else {
+    console.warn('PWA support: service worker not supported 😣. Skipping PWA...');
   }
-
-  navigator.serviceWorker.register('/pwa-serviceworker.js')
-  .then(function(registration) {
-    console.log('PWA support: Service worker successfully registered in scope 🏼:', registration.scope);
-  })
-  .catch(function(error) {
-    console.error('PWA support: Service worker registration failed! 😱', error)
-  });
 })
