c913869df7ddb130543d4ead59fed4d91ec751a8
Oliver Lanz <oliver@Olivers-MacBook-Pro.local>
2019-11-01 10:10:10 +0100

    Updated all libraries as from template from dotnet core 3

--- backend/src/Pages/Error.cshtml.cs
+++ backend/src/Pages/Error.cshtml.cs
@@ -5,15 +5,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace backend.Pages {
+namespace backend.Pages
+{
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public class ErrorModel : PageModel {
+    public class ErrorModel : PageModel
+    {
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        public void OnGet() {
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
--- backend/src/Pages/Index.cshtml.cs
+++ backend/src/Pages/Index.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace backend.Pages {
     public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+
         public string Message { get; set; }
 
+        public IndexModel(ILogger<IndexModel> logger) {
+            _logger = logger;
+        }
+
         public void OnGet() {
-            Message = "Login.";
+           Message = "Login.";
         }
 
         public string GetRandomPlayerName() {
