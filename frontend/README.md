0cd5ae4918059bbd197a7f77a934e9a93afc3ae6
Oliver C. Lanz <olilanz@mac.com>
2020-10-13 10:17:38 +0200

    Avoid 'regeneratorRuntime is not defined' error

--- frontend/README.md
+++ frontend/README.md
@@ -9,24 +9,34 @@ Part of the [mini-game](~/README.md).
 
 The following commands will get you started:
 
-| Command | Description |
-|---------|-------------|
-| `npm i -g npm` | Update npm to required version.|
-| `npm install` | Install dependencies.|
-| `npm start` | Builds game and startes server. Connect to http://localhost:1234. |
-| `npm run build` | Builds game. |
+| Command         | Description                                                       |
+| --------------- | ----------------------------------------------------------------- |
+| `npm i -g npm`  | Update npm to required version.                                   |
+| `npm install`   | Install dependencies.                                             |
+| `npm start`     | Builds game and startes server. Connect to http://localhost:1234. |
+| `npm run build` | Builds game.                                                      |
 
 
 ## Updating packages
 
-| Command | Description |
-|---------|-------------|
-| `npm outdated` | Shows which packages have new releases that could possibly be upgraded to. |
-| `npm upgrade -g` | Updates all top level packages to the latest version. |
+| Command          | Description                                                                |
+| ---------------- | -------------------------------------------------------------------------- |
+| `npm outdated`   | Shows which packages have new releases that could possibly be upgraded to. |
+| `npm upgrade -g` | Updates all top level packages to the latest version.                      |
 
 
-## Thaanks to
+## Thanks to
 
 * Richard Davey from [Photon Storm](https://twitter.com/photonstorm) for his dedication to creating Phaser
 * Nate from [EsotericSoftware](http://esotericsoftware.com/blog/building-spine) for bringing skeleton animations to all platforms.
 * Yannick for his providing his sample projects on [github](https://github.com/yandeu)
+
+# Considertations
+
+## Supported browsers
+
+The list of supported browsers is defined in `package.json`. The intention was to support a broad spectrum of active browsers, 
+and then have the polyfiller handle the compatibility issues. Unfortunately I hit the `regeneratorRuntime is not defined` error
+with IE 11. It is for that reason excluded form the list.
+
+Reference: [Parcel, how to fix the `regeneratorRuntime is not defined` error](https://flaviocopes.com/parcel-regeneratorruntime-not-defined/)
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,34 +1,33 @@
-    import { Game } from './game';
-    import { ExternalGameConfig, GameMode } from './externalgameconfig';
+import { Game } from './game';
+import { ExternalGameConfig, GameMode } from './externalgameconfig';
 
-    // when the page is loaded, create our game instance
-    window.onload = () => {
-        'use strict';
+// when the page is loaded, create our game instance
+window.onload = () => {
+    'use strict';
 
-        // read the game configuration from the URL query string
-        // todo: let url = ew URL(window.URLSearchParams)
-        // initialize the game
-        let config = buildExternalConfig(new URLSearchParams(window.location.search));
-        let game = new Game("game-ganvas", config);
+    // read the game configuration from the URL query string
+    // todo: let url = ew URL(window.URLSearchParams)
+    // initialize the game
+    let config = buildExternalConfig(new URLSearchParams(window.location.search));
+    let game = new Game("game-ganvas", config);
+    game.scale.resize(window.innerWidth, window.innerHeight);
+
+    // make sure that the game resizes when the browser resizes
+    window.addEventListener('resize', () => {
         game.scale.resize(window.innerWidth, window.innerHeight);
-        
-        // make sure that the game resizes when the browser resizes
-        window.addEventListener('resize', () => { 
-            game.scale.resize(window.innerWidth, window.innerHeight);
-        }, false);
-    }
+    }, false);
+}
 
-    function buildExternalConfig(searchParams: URLSearchParams): ExternalGameConfig {
-        // read params
-        let playerName: string | null = searchParams.get("playerName");
-        let gameMode: string | null = searchParams.get("gameMode");
+function buildExternalConfig(searchParams: URLSearchParams): ExternalGameConfig {
+    // read params
+    let playerName: string | null = searchParams.get("playerName");
+    let gameMode: string | null = searchParams.get("gameMode");
 
-        // defaults
-        let config: ExternalGameConfig = {
-            playerName: playerName ? playerName : "noname", 
-            gameMode: gameMode == "server" ? GameMode.server : GameMode.normal
-        };
+    // defaults
+    let config: ExternalGameConfig = {
+        playerName: playerName ? playerName : "noname",
+        gameMode: gameMode == "server" ? GameMode.server : GameMode.normal
+    };
 
-        return config;
-    }
-    
+    return config;
+}
